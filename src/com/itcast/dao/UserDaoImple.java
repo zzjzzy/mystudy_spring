9ae11908721abcf4ce762f3592563a8e486c8a7c
zzj <zhangzjzzj@foxmail.com>
2019-05-11 23:14:00 +0800

    spring commit 20190511-23:13

--- src/com/itcast/dao/UserDaoImple.java
+++ src/com/itcast/dao/UserDaoImple.java
@@ -1,9 +1,15 @@
 package com.itcast.dao;
 
 public class UserDaoImple implements UserDao {
+	
+	String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public void save() {
-		System.out.println("UserDaoImpl执行了！");
+		System.out.println("UserDaoImpl执行了！"+name);
 
 	}
 
